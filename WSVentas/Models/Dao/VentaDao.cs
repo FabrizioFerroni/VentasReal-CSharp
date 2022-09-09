7b13d97c30ce041d8dc32e1d4d502d6340db2c8c
Fabrizio Ferroni <fabrizioferroni@outlook.com>
2022-09-08 21:02:07 -0300

    Se hizo validacion modelo ventas, video 20

--- WSVentas/Models/Dao/VentaDao.cs
+++ WSVentas/Models/Dao/VentaDao.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,13 @@ namespace WSVentas.Models.Dao
 {
     public class VentaDao
     {
-        public int IdCliente { get; set; }
+        [Required]
+        [Range(1, Double.MaxValue, ErrorMessage = "El valor del IdCliente debe ser mayor que 0")]
+        [ExisteCliente(ErrorMessage = "El cliente no éxiste")]
+        public int IdClienExisteClienteAttributete { get; set; }
 
-        public decimal Total { get; set; }
-
+        [Required]
+        [MinLength(1,ErrorMessage = "Deben existir conceptos")]
         public List<Concepto> Conceptos { get; set; }
 
         public VentaDao()
@@ -26,4 +30,18 @@ namespace WSVentas.Models.Dao
         public decimal Importe { get; set; }
         public int IdProducto { get; set; }
     }
+    #region Validaciones
+    public class ExisteClienteAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            int idClt = (int)value;
+            using (var db = new Models.VentasRealContext())
+            {
+                if (db.Cliente.Find(idClt) == null) return false;
+            }
+            return true;
+        }
+    }
+    #endregion
 }
